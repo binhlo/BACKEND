36d62c6b43bb07e9e67f694025d57a034ead33dd
binhlo <160563459+binhlo@users.noreply.github.com>
2025-06-16 13:40:50 +0700

    Lab 1.1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap so: ");
+        int n = int.Parse(Console.ReadLine());
+
+        bool laNT = true;
+
+        if (n < 2)
+        {
+            laNT = false;
+        }
+        else
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    laNT = false;
+                    break;
+                }
+            }
+        }
+
+        if (laNT)
+            Console.WriteLine(n + " la so nguyen to");
+        else
+            Console.WriteLine(n + " khong phai so nguyen to");
+    }
+}
